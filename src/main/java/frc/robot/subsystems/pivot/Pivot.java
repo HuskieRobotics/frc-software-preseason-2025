7f5bb9caf023def605399de82cd18cac731766b5
adamkuc9 <agkuc@stu.naperville203.org>
2025-10-01 19:33:11 -0500

    Made a mistake with git, hopefully fixing it

--- src/main/java/frc/robot/subsystems/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/pivot/Pivot.java
@@ -1,18 +1,5 @@
 package frc.robot.subsystems.pivot;
 
-<<<<<<< HEAD
-
-
-public class Pivot extends SubsystemsBase {
-    private PivotIO pivotio;
-    
-
-    
-
-
-}
-=======
 public class Pivot {
     
 }
->>>>>>> 3d7820de8397d1e77d9d1309dfb789dfc7c9c13a
