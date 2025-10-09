ce0c4f3779c06e1c10c932f2d9f95c6e5f903454
Guru Shanmukha Pranav Sanka <161491711+Gssanka@users.noreply.github.com>
2025-10-08 20:48:00 -0500

    Started working on talonfx

---
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorIOTalonFX.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems.elevator;
+
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static frc.robot.subsystems.Elevator.ElevatorConstants.*;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.TorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Temperature;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import frc.lib.team254.Phoenix6Util;
+import frc.lib.team3015.subsystem.FaultReporter;
+import frc.lib.team3061.RobotConfig;
+import frc.lib.team3061.sim.VelocitySystemSim;
+import frc.lib.team6328.util.LoggedTunableNumber;
+import frc.robot.Constants;
+
