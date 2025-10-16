2f841fefb7ebc7f0143db0dbd2a538186a2c2987
Charlotte-Cao344 <cecao@stu.naperville203.org>
2025-10-15 19:06:11 -0500

    worked on pivot subsystem

--- src/main/java/frc/robot/subsystems/pivot/PivotConstants.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotConstants.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems.pivot;
 
 public class PivotConstants {
+
+    public static final int PIVOT_LEAD_MOTOR_ID = 80;
+    public static final int PIVOT_FOLLOWER_MOTOR_ID_1 = 81;
+    public static final int PIVOT_FOLLOWER_MOTOR_ID_2 = 83;
+    public static final int PIVOT_FOLLOWER_MOTOR_ID_3 = 84;
+
+    public static final int PIVOT_ENCODER_ID = 0;
     
+    public static final double PIVOT_KG = 0;
+    public static final double PIVOT_KS = 0;
+    public static final double PIVOT_KV = 0;
+    public static final double PIVOT_KA = 0;
+    public static final double PIVOT_KP = 0;
+    public static final double PIVOT_KI = 0;
+    public static final double PIVOT_KD = 0;
+    public static final double PIVOT_KA_EXPO = 0;
+    public static final double PIVOT_KV_EXPO = 0;
+
 }
--- src/main/java/frc/robot/subsystems/pivot/PivotIO.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIO.java
@@ -1,6 +1,29 @@
 package frc.robot.subsystems.pivot;
 
+import edu.wpi.first.units.measure.Angle;
+import org.littletonrobotics.junction.AutoLog;
+
 public interface PivotIO {
 
-    @AutoLog
+  // The inputs class for a subsystem usually contains the stator and supply currents, temperature,
+  // voltage (or current), and, depending on the control mode, additional fields related to position
+  // or velocity (both measured and reference). The first property is always `connected` and logs if
+  // each device is reachable. Due to logging limitations, properties cannot be a subtype of
+  // Measure. Therefore all properties are suffix with their unit to mitigate bugs due to unit
+  // mismatches.
+  @AutoLog
+  public static class PivotIOInputs {
+    double voltageSupplied = 0.0;
+    double statorCurrentAmps = 0.0;
+    double supplyCurrentAmps = 0.0;
+    double angleMotorClosedLoopErrorAngleDegrees = 0.0;
+    double closedLoopReferenceAngleDegrees = 0.0;   
+    double angleMotorTemperatureCelsius = 0.0;
+    double positionAngleDegrees = 0.0;  
+    
+    boolean connected = false;
+  }
+
+  public default void setVoltage(double voltage) {}
+  public default void setAngle(Angle angle) {}
 }
